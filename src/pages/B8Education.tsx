8653cdc50d9231313ff644dd6de0e1c3095dcb59
Humza Butt <Humzab1711@hotmail.com>
2025-02-08 22:06:20 +0000

    Education fix

--- src/pages/B8Education.tsx
+++ src/pages/B8Education.tsx
@@ -65,7 +65,7 @@ export default function B8Education() {
           width="100%"
           height="300"
           style={{ border: 0 }}
-          allowFullScreen=""
+          allowFullScreen
           loading="lazy"
         ></iframe>
       </section>
