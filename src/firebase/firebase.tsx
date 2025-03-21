9284a7fb466472c8a6ca225d841a55f27354841f
Humza Butt <Humzab1711@hotmail.com>
2025-03-21 16:34:37 +0000

    Comment out unused Firebase configuration variables for clarity and maintainability in firebase.tsx.

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -16,6 +16,13 @@ const getEnvVariable = (key: string): string => {
 };
 
 const firebaseConfig = {
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
   apiKey: getEnvVariable('VITE_FIREBASE_API_KEY'),
   authDomain: getEnvVariable('VITE_FIREBASE_AUTH_DOMAIN'),
   projectId: getEnvVariable('VITE_FIREBASE_PROJECT_ID'),
