8da211db04759b49ef630017a0d3e9e813840b41
Humza Butt <Humzab1711@hotmail.com>
2025-07-02 20:11:30 +0100

    Add hobbies and hobbiesByCategory constants: Introduce a comprehensive list of hobbies and categorize them into distinct groups for better organization and accessibility. This addition enhances the application's ability to manage and display hobby-related data effectively.

---
+++ src/constants/hobbiesByCategory.ts
@@ -0,0 +1,67 @@
+const hobbiesByCategory = {
+  Sports: [
+    'Football', 'Cricket', 'Rugby', 'Tennis', 'Cycling', 'Running', 'Swimming', 'Hiking', 'Basketball', 'Baseball', 'Volleyball', 'Table Tennis', 'Badminton', 'Golf', 'Rock Climbing', 'Skateboarding', 'Surfing', 'Skiing', 'Snowboarding', 'Ice Skating', 'Roller Skating', 'Pilates', 'Tai Chi', 'Karate', 'Judo', 'Taekwondo',
+  ],
+  Music: [
+    'Singing', 'Opera Singing', 'Choir', 'Guitar', 'Piano', 'Violin', 'Drums', 'Saxophone',
+  ],
+  Dance: [
+    'Dancing', 'Ballet', 'Jazz Dance', 'Hip Hop Dance', 'Ballroom Dancing',
+  ],
+  Art: [
+    'Drawing', 'Painting', 'Photography', 'Street Photography', 'Wildlife Photography', 'Portrait Photography', 'Digital Art', 'Animation', 'Calligraphy', 'Sculpture', 'Pottery',
+  ],
+  Writing: [
+    'Writing', 'Poetry', 'Creative Writing', 'Journalism',
+  ],
+  Crafts: [
+    'DIY Crafts', 'Woodworking', 'Metalworking', 'Jewelry Making', 'Knitting', 'Crocheting', 'Sewing', 'Quilting', 'Interior Design', 'Model Building',
+  ],
+  FoodAndDrink: [
+    'Cooking', 'Baking', 'International Cuisine', 'BBQ & Grilling', 'Wine Tasting', 'Beer Brewing', 'Cake Decorating', 'Bread Making',
+  ],
+  Gardening: [
+    'Gardening', 'Urban Gardening', 'Flower Arranging',
+  ],
+  Travel: [
+    'Travel', 'Backpacking', 'Road Trips', 'Wilderness Camping', 'RV Camping', 'Beach Camping',
+  ],
+  Volunteering: [
+    'Volunteering', 'Animal Rescue', 'Environmental Conservation', 'Teaching',
+  ],
+  Games: [
+    'Chess', 'Checkers', 'Board Games', 'Bridge', 'Mahjong', 'Dungeons & Dragons', 'Video Games', 'PC Gaming', 'Console Gaming', 'Mobile Gaming',
+  ],
+  Collecting: [
+    'Collecting', 'Stamp Collecting', 'Coin Collecting', 'Comic Book Collecting', 'Record Collecting',
+  ],
+  Nature: [
+    'Bird Watching', 'Nature Photography',
+  ],
+  BloggingVlogging: [
+    'Blogging', 'Podcasting', 'Vlogging', 'Web Design',
+  ],
+  OutdoorActivities: [
+    'Fishing', 'Deep Sea Fishing', 'Fly Fishing', 'Ice Fishing', 'Camping',
+  ],
+  Mindfulness: [
+    'Yoga', 'Meditation', 'Mindfulness',
+  ],
+  Tech: [
+    'Drone Flying', 'Remote Control Cars',
+  ],
+  Science: [
+    'Astronomy', 'Genealogy',
+  ],
+  Performance: [
+    'Stand-up Comedy', 'Acting', 'Musical Theater', 'Magic Tricks', 'Juggling',
+  ],
+  Language: [
+    'Language Learning',
+  ],
+  Other: [
+    'Fashion', 'Film & TV', 'Other',
+  ],
+};
+
+export default hobbiesByCategory; 
