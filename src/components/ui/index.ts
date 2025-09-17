1de8f87aed6b65dfd604cbdb28a95e45a026d049
Humza Butt <Humzab1711@hotmail.com>
2025-09-18 00:59:27 +0100

    Add UI Components Export File and Update Imports
    - Created a new index file in the UI components directory to streamline exports of various components including Navbar, Footer, and PasswordStrengthMeter.
    - Updated import statements in MobileSignInPage and SignInPage to utilize the new index file for importing PasswordStrengthMeter, improving code organization and maintainability.

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+// UI Components exports
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as HamburgerMenu } from './HamburgerMenu';
+export { default as ContactForm } from './ContactForm';
+export { default as JoinOurTeam } from './JoinOurTeam';
+export { default as SocialChannels } from './SocialChannels';
+export { UserRoleBadge } from './UserRoleBadge';
+export { default as PasswordStrengthMeter } from './PasswordStrengthMeter';
